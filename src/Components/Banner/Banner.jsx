05b44806ec7c074bb7ff29f9eff77bc15b7f9c9d
Sreerag <sreeragmonu62@gmail.com>
2024-01-18 18:03:22 +0530

    project completed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,26 +6,28 @@ import { useState } from 'react'
 
 
 function Banner() {
-  const [movie, setMovie] = useState()
+  const [movie, setMovie] = useState([])
+  const index=movie[Math.floor(Math.random()*movie.length)]
+  console.log(movie);
   useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-    console.log(response.data.results[0]);
-    setMovie(response.data.results[0])
+    console.log(response.data.results);
+    setMovie(response.data.results)
     })
    
   }, [])
   
   return (
     <div 
-    style={{backgroundImage:`url(${movie? imageUrl+movie.backdrop_path :""})`}}
+    style={{backgroundImage:`url(${index? imageUrl+index.backdrop_path :""})`}}
     className='banner'>
         <div className='content'>
-           <h1 className='title'>{movie?.title}</h1>
+           <h1 className='title'>{index?.title}</h1>
            <div className='banner_buttons'>
             <button className='button'>play</button>
             <button className='button'>My list</button>
            </div>
-           <h1 className='description'>{movie?.overview}</h1>
+           <h1 className='description'>{index?.overview}</h1>
         </div>
         <div className="fade"></div>
     </div>
