5cbd35e0a70030595f7c97bad9ec0109236ec157
Sreerag <sreeragmonu62@gmail.com>
2024-01-18 12:25:37 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NavBar/NavBar';
-import {action,comedy} from './url'
+import {action,comedy,adventure,drama,crime,family,fantasy,history,horror} from './url'
 import RowPost from './Components/RowPost/RowPost';
+
 
 function App() {
   return (
@@ -12,6 +13,14 @@ function App() {
       <Banner/>
       <RowPost url={comedy} title='Comedy'/>
       <RowPost url={action} title='Action' isSmall />
+      <RowPost url={adventure} title='Adventure' isSmall />
+       <RowPost url={drama} title='Drama' isSmall />
+       <RowPost url={crime} title='Crime' isSmall />
+
+      <RowPost url={family} title='Family' isSmall /> 
+      <RowPost url={fantasy} title='Fantasy' isSmall /> 
+      <RowPost url={history} title='History' isSmall /> 
+      <RowPost url={horror} title='Horror' isSmall /> 
       
     </div>
   );
--- src/url.js
+++ src/url.js
@@ -1,3 +1,10 @@
 import {API_KEY} from './constants/Permanent'
 export const comedy = `discover/tv?api_key=${API_KEY}&with_networks=35`
 export const action =`/discover/movie?api_key=${API_KEY}&with_genres=28`
+export const adventure =`/discover/movie?api_key=${API_KEY}&with_genres=12`
+export const drama =`/discover/movie?api_key=${API_KEY}&with_genres=18`
+export const crime =`/discover/movie?api_key=${API_KEY}&with_genres=80`
+export const family =`/discover/movie?api_key=${API_KEY}&with_genres=10751`
+export const fantasy =`/discover/movie?api_key=${API_KEY}&with_genres=14`
+export const history =`/discover/movie?api_key=${API_KEY}&with_genres=36`
+export const horror =`/discover/movie?api_key=${API_KEY}&with_genres=27`
